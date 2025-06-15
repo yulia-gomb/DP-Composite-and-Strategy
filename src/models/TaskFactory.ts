1b9314d438260ba7d8f79412e679cf66b576babc
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-16 01:50:09 +0300

    Add logic to task loader for subtasks

--- src/models/TaskFactory.ts
+++ src/models/TaskFactory.ts
@@ -3,19 +3,22 @@ import { SimpleTask } from './SimpleTask';
 import { AgileStrategy } from './AgileStrategy';
 import { WaterfallStrategy } from './WaterfallStrategy';
 import { RUPStrategy } from './RUPStrategy';
+import { CompositeTask } from "./CompositeTask.ts";
 
 export class TaskFactory {
-    static createTask(name: string, type: string): Task {
+    static createTask(name: string, type: string, hasSubTasks: boolean): Task {
+
+        if (hasSubTasks) {
+            return new CompositeTask(name); // Use CompositeTask if subtasks exist
+        }
+
         switch (type) {
             case 'development':
                 return new SimpleTask(name, new AgileStrategy());
-
             case 'testing':
                 return new SimpleTask(name, new WaterfallStrategy());
-
             case 'design':
                 return new SimpleTask(name, new RUPStrategy());
-
             default:
                 throw new Error(`Unknown task type: "${type}"`);
         }

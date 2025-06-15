7c8facb69ed683e17120c13c7d605a95015822a5
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-16 00:35:07 +0300

    Add testing changing tasks statuses

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
+import {TaskFactory} from "./models/TaskFactory.ts";
 
+class App {
+    private taskLogs: string[] = []; // Array to store task execution logs
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <p>
-      Hello
-    </p>
-  </div>
-`
+    private updateLogs(message: string): void {
+        this.taskLogs.push(message);
+        this.renderLogs();
+    }
+
+    private renderLogs(): void {
+        const logContainer = document.getElementById('logs');
+        if (logContainer) {
+            logContainer.innerHTML = ''; // Clear previous logs
+            this.taskLogs.forEach(log => {
+                const logItem = document.createElement('div');
+                logItem.textContent = log;
+                logContainer.appendChild(logItem);
+            });
+        }
+    }
+
+    run(): void {
+        const devTask = TaskFactory.createTask('Frontend Development', 'development');
+        devTask.setStatusCallback((msg) => this.updateLogs(msg));
+
+        const testTask = TaskFactory.createTask('Unit Testing', 'testing');
+        testTask.setStatusCallback((msg) => this.updateLogs(msg));
+
+        const designTask = TaskFactory.createTask('Design', 'design');
+        designTask.setStatusCallback((msg) => this.updateLogs(msg));
+
+        devTask.execute();
+        testTask.execute();
+        designTask.execute();
+    }
+}
+
+const app = new App();
+app.run();

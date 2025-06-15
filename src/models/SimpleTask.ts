8887942a7be86327ddd6d7da54ff9e76b2472c68
Yuliya Grasevich <grasevich.yuliya@student.ehu.lt>
2025-06-15 23:33:47 +0300

    Add class SimpleTask

---
+++ src/models/SimpleTask.ts
@@ -0,0 +1,19 @@
+import { Task } from './Task';
+import type { TaskStrategy } from "./TaskStrategy.ts";
+
+
+export class SimpleTask extends Task {
+    constructor(name: string, strategy: TaskStrategy) {
+        super(name, strategy);
+    }
+
+    execute(): void {
+        console.log(`Executing simple task: "${this.name}"`);
+        if (this.strategy) {
+            this.strategy.execute(this);
+        } else {
+            console.log(`No execution strategy defined for task: "${this.name}"`);
+        }
+        this.markComplete();
+    }
+}
